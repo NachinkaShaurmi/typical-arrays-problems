ed58b37712db975fdb51548604abd1e70c2ca59f
Yuriy <agent250691@yandex.ru>
2020-09-29 11:50:26 +0300

    feat: compleat task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (arguments.length === 0 || array.length == 0) return 0;
+    let minValue = array[0];
+    for (el of array) {
+        if (el < minValue) minValue = el;
+    }
+    return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length === 0 || array.length == 0) return 0;
+    let maxValue = array[0];
+    for (el of array) {
+        if (el > maxValue) maxValue = el;
+    }
+    return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length === 0 || array.length == 0) return 0;
+
+    return array.reduce(((sum, el) => sum + el), 0) / array.length;
 }
